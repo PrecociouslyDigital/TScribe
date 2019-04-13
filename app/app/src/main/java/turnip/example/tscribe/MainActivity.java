6bb67114150bd41b5db7cd6bc2f6723878cc5f66
Neha Nagvekar <nneha@live.com>
2019-04-13 11:58:56 -0700

    Create android project

---
+++ app/app/src/main/java/turnip/example/tscribe/MainActivity.java
@@ -0,0 +1,13 @@
+package turnip.example.tscribe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
