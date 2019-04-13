7d40731730bbd7499cf40925385fd22f83869607
Michelle Lin <linm23@cs.washington.edu>
2019-04-13 15:38:02 -0700

    Texting UI works

--- app/app/src/main/java/turnip/example/tscribe/MichelleActivity.java
+++ app/app/src/main/java/turnip/example/tscribe/MichelleActivity.java
@@ -7,33 +7,116 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import java.util.Random;
+
 public class MichelleActivity extends AppCompatActivity {
 
-    ListView messages;
-    EditText editText;
-    ImageButton send;
+    private EditText editText;
+    private MessageAdapter messageAdapter;
+    private ListView messagesView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        messages = (ListView) findViewById(R.id.messages_view);
+
         editText = (EditText) findViewById(R.id.editText);
-        send = (ImageButton) findViewById(R.id.send);
+
+        messageAdapter = new MessageAdapter(this);
+        messagesView = (ListView) findViewById(R.id.messages_view);
+        messagesView.setAdapter(messageAdapter);
+
     }
 
     public void sendMessage(View view) {
         String message = editText.getText().toString();
         if (message.length() > 0) {
-            // send out message
+            // send message to server
             editText.getText().clear();
         }
     }
 
-    private View.OnClickListener sendButtonClick = new View.OnClickListener(){
-        public void onClick(View v){
-            sendMessage(v);
-        }
-    };
+    // connect to server
+    public void onOpen() {
+        System.out.println("Connected to room");
+    }
 
+    // failed to connect to server
+    public void onOpenFailure(Exception ex) {
+        System.err.println(ex);
+    }
+
+    // what happens when message is sent
+    public void onMessage(Message receivedMessage) {
+        // send stuff to server
+        final Message message = new Message(receivedMessage.getText(), receivedMessage.isMine());
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageAdapter.add(message);
+                messagesView.setSelection(messagesView.getCount() - 1);
+            }
+        });
+//        try {
+//            final Message message = new Message(receivedMessage.getText(), receivedMessage.isMine());
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    messageAdapter.add(message);
+//                    messagesView.setSelection(messagesView.getCount() - 1);
+//                }
+//            });
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    private String getRandomName() {
+        String[] adjs = {"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"};
+        String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
+        return (
+                adjs[(int) Math.floor(Math.random() * adjs.length)] +
+                        "_" +
+                        nouns[(int) Math.floor(Math.random() * nouns.length)]
+        );
+    }
+
+    private String getRandomColor() {
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer("#");
+        while(sb.length() < 7){
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString().substring(0, 7);
+    }
 }
+
+//class MemberData {
+//    private String name;
+//    private String color;
+//
+//    public MemberData(String name, String color) {
+//        this.name = name;
+//        this.color = color;
+//    }
+//
+//    public MemberData() {
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getColor() {
+//        return color;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "MemberData{" +
+//                "name='" + name + '\'' +
+//                ", color='" + color + '\'' +
+//                '}';
+//    }
+
+
