42d2a3004672a615ebd3d1d7652d6bd9f9f8e46c
Rachel <sittr@cs.washington.edu>
2019-04-13 15:35:44 -0700

    Committed

--- app/app/src/main/java/turnip/example/tscribe/CallFragment.java
+++ app/app/src/main/java/turnip/example/tscribe/CallFragment.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class CallFragment extends Fragment {
     private static final String TAG = "CallFragment";
