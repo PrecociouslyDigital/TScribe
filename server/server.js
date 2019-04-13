1b3006f4bf9df0301ceca8c4c7c0c7a44c8ca68b
precociouslydigital <fellfromatree@gmail.com>
2019-04-13 14:57:02 -0700

    WETWERWE

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cluster = require('cluster');
 const { execFile } = require('child_process');
 const app = express();
 const port = 3000;
-const canonHost = ;
+const canonHost = "tscribe.crabdance.com";
 
 var shardIncrement = 1;
 
@@ -17,16 +17,17 @@ app.post('/call', (req, res) => {
     console.log(req.body);
     await createShard(req.body, {
         host: canonHost,
-        port: port+(shardIncrement++);
+        port: port+(shardIncrement++),
+        token: "idk",
     });
     console.log(`spun up shard at ${canonHost}:${port + shardIncrement}`);
-    res(`http://${canonHost}:${port + shardIncrement}`);
+    res(JSON.stringify(`ws://${canonHost}:${port + shardIncrement}`));
 })
 
 app.listen(port, () => console.log(`Master Server listening on port ${port}!`))
 
-function createShard({number, opening}, {host, port}){
-    const child = execFile('./shard.js', [number, opening, host, port], (error, stdout, stderr) => {
+function createShard({callNumber, userNumber, opening}, {host, port, token}){
+    const child = execFile('./shard.js', [callNumber, userNumber, opening, host, port, token], (error, stdout, stderr) => {
         if (error) {
             throw error;
         }
--- server/shard.js
+++ server/shard.js
@@ -1,2 +1,53 @@
-const [number, opening, url, port] = process.argv;
-const 
+const [callNumber, userNumber, opening, url, port] = process.argv;
+const expressWs = require('express-ws')(app);
+
+var wsSingleton;
+
+var wsProcessQueue = [];
+
+var startingMessage;
+
+app.ws('/', function(ws, req) {
+    // TODO: Token Auth;
+    wsSingleton = ws;
+    wsSingleton.on('message', (msg) => wsProcessQueue.push(processWsMessage(JSON.parse(msg))));
+    wsSingleton.on('close', closeSession);
+});
+
+app.get('/prompt', (req, res) => {
+    wsSingle
+});
+app.get('/start', (req, res) => {
+
+});
+app.get('/partial', (req, res) => wsSingleton))
+
+function processWsMessage(msg){
+    
+}
+function closeSession(){
+    if()
+}
+
+const mainLoop = (msg, redirect='/prompt')=>`
+<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Gather action="/prompt" method="GET" input="speech">
+        <Say>
+            ${msg}
+        </Say>
+    </Gather>
+    <Redirect method='GET'>${redirect}</Redirect>
+</Response>`;
+const start = (say, redirect='/prompt')=>`
+<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say>${say}</Say>
+    <Redirect method='GET'>${redirect}</Redirect>
+</Response>`;
+
+const end = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say>User has hung up. Goodbye!</Say>
+    <Hangup/>
+</Response>`
